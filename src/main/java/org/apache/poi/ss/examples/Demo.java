e062e1dae4cfc3eb2a0b11a57d0d7f078d4d7ea8
wangjc7 <wang.jc@aliyun.com>
2020-06-07 12:21:51 +0800

    add file

---
+++ src/main/java/org/apache/poi/ss/examples/Demo.java
@@ -0,0 +1,12 @@
+package org.apache.poi.ss.examples;
+
+/**
+ * @author wangjc
+ * @email wangjc161@chinaunicom.cn
+ * @date 2020/6/7 12:20 PM
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world.");
+    }
+}
