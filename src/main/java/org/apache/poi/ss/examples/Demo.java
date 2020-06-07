aaf0ceda89a819e53b825ab56908d795f59fb502
wangjc7 <wang.jc@aliyun.com>
2020-06-07 12:40:09 +0800

    good job

--- src/main/java/org/apache/poi/ss/examples/Demo.java
+++ src/main/java/org/apache/poi/ss/examples/Demo.java
@@ -8,5 +8,6 @@ package org.apache.poi.ss.examples;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world.");
+        System.out.println("very good.");
     }
 }
